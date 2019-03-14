7b3894ee2a77dc7632665b720d753b8a7d6f479d
Michael Bey <michael.bey@stu.bmcc.cuny.edu>
2019-03-14 00:10:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+
+const head = 42
+const feetPerBlock = 264
+
+function distanceFromHqInBlocks(block){
+  return Math.abs(block - head)
+}
+
+function distanceFromHqInBlocks(block){
+  return distanceFromHqInBlocks(block) * feetPerBlock
+}2
